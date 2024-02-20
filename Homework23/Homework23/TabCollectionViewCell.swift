999809a64f95236b043d7f255b6956336bb238ae
Timur Mir <mirtimur1@gmail.com>
2024-02-20 21:35:04 +0300

    Homework

---
+++ Homework23/Homework23/TabCollectionViewCell.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class TabCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var websiteLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
